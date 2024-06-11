bfe726935f6f242c88be50600d78a64128ce3931
luiscacostas <acosta.luisc15@gmail.com>
2024-06-11 17:45:51 +0200

    creacion de funcion de validacion

--- script.js
+++ script.js
@@ -1,40 +1,96 @@
-//**************************VARIABLES*******************************
 const url = 'https://opentdb.com/api.php?amount=10';
 const quiz = document.querySelector('.quiz');
-const index = 0;
 let options = [];
+let results = [];
+let index = 0;
+let respuestas = [];
+let fecha = new Date().toDateString() //Fecha para puntuacion
+console.log(fecha)
 
 
+quiz.addEventListener('click', (ev)=>{
+    ev.preventDefault()
 
-//**************************EVENTS**********************************
-
-//**************************FUNCTIONS*******************************
+    if(ev.target.tagName === 'BUTTON'){
+        const valueOption = ev.target.value
+        console.log(valueOption)
+        validateResponse(valueOption, ev.target)
+    }
+    if (ev.target.id === 'siguiente') {
+        index++;
+        printQuiz(results, index)
+    }
+})
 
 const getData = async () => {
     try {
         const resp = await fetch(url);
         if (resp.ok) {
             const data = await resp.json();
-            const results = data.results;
+            results = data.results;
             printQuiz(results, index);
             console.log(results);
         } else {
-            throw 'error';
+            throw new Error('Error al obtener los datos');
         }
-    } catch {
-        throw error;
+    } catch (error) {
+        console.error(error);
     }
 }
 
 const printQuiz = (results, i) => {
-    options = results[i].incorrect_answers;
-    options.push(results[i].correct_answer);
-    
+    quiz.innerHTML = '';
+    options = [...results[i].incorrect_answers, results[i].correct_answer];
+    console.log(options);
+    const pregunta = document.createElement('H3')
+    pregunta.textContent = results[i].question;
+    quiz.append(pregunta)
 
+    options.forEach((opc, index) => {
+        const buttonOption = document.createElement('BUTTON')
+        buttonOption.textContent = opc;
+        buttonOption.setAttribute('id', `option${index+1}`)
+        buttonOption.classList.add(`option${index+1}`)
+        buttonOption.value = opc
+        console.log(buttonOption)
+        quiz.append(buttonOption)
+    });
+    const buttonNext = document.createElement('INPUT')
+    buttonNext.setAttribute('type', 'submit')
+    buttonNext.value= 'Siguiente'
+    buttonNext.setAttribute('id', 'siguiente')
+    quiz.append(buttonNext)
 };
 
+const validateResponse = (value, button)=>{
+    const buttons = document.querySelectorAll('button')
+    if(value === results[index].correct_answer){
+        button.classList.add('styleOptionActive')
+        respuestas.push('Correcta');
+    }else{
+        respuestas.push('Incorrecta');
+        button.classList.add('styleOptionInactive')
+       
+        const botonCorrecto = [...buttons].find((element)=> element.value === results[index].correct_answer)
+        if(botonCorrecto){
+            botonCorrecto.classList.add('styleOptionActive')
+        }
+    }
 
+    const botones = document.querySelectorAll('button');
+    botones.forEach(boton => {
+        boton.disabled = true;
+    });
+    printResults()
+    return respuestas;
+}
+
+const printResults = () => {
+    console.log(respuestas);//Utilizar para graficas y resultado final Almacenar local
+}
 
 getData()
-// .then(resp => console.log(resp))
-// .catch(error => console.error(error))
+
+//animacion tiempo
+//animacion botones
+
